6a37e6b713cbb30645d1194ba5f335ee83b04038
BuildTools <unconfigured@null.spigotmc.org>
2022-09-17 18:00:28 +0900

    alias 등록

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,19 @@
 const { defineConfig } = require('@vue/cli-service')
+const path = require('path')
+
 module.exports = defineConfig({
   devServer: {
     allowedHosts: ['downbit.r-e.kr']
   },
   transpileDependencies: true,
-  lintOnSave: false
+  lintOnSave: false,
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@': path.join(__dirname, 'src/'),
+        '@compo': path.join(__dirname, 'src/components/'),
+        '@views': path.join(__dirname, 'src/views/'),
+      },
+    },
+  },
 })
