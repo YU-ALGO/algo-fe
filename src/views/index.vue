7b9af37a9e368ae27fb74964b85f2d2a1ada96d6
choijohn7 <choijohn7@naver.com>
2022-11-21 22:32:09 +0900

    메인 페이지 수정
    공지사항 및 TOP 게시글 구현
    bootstrap icon <i> 태그 사용

--- src/views/index.vue
+++ src/views/index.vue
@@ -10,60 +10,28 @@
                 <div class="card-body">
                   <table class="table table-hover" style="text-align: center">
                     <thead>
-                    <tr>
-                      <th>번호</th>
-                      <th>제목</th>
-                      <th>작성자</th>
-                      <th>작성일</th>
-                      <th>조회수</th>
-                      <th>추천수</th>
-                    </tr>
+                      <tr>
+                        <th>번호</th>
+                        <th>제목</th>
+                        <th>작성자</th>
+                        <th>작성일</th>
+                        <th>조회수</th>
+                        <th>추천수</th>
+                      </tr>
                     </thead>
                     <tbody>
-                    <tr>
-                      <th scope="row">1</th>
-                      <td><a
-                          href=""
-                          target='_blank'>테스트</a> [9]
-                      </td>
-                      <td>작성자</td>
-                      <td>2022.09.21</td>
-                      <td>111</td>
-                      <td>111</td>
-                    </tr>
-                    <tr>
-                      <th scope="row">1</th>
-                      <td><a
-                          href=""
-                          target='_blank'>테스트</a> [9]
-                      </td>
-                      <td>작성자</td>
-                      <td>2022.09.21</td>
-                      <td>111</td>
-                      <td>111</td>
-                    </tr>
-                    <tr>
-                      <th scope="row">1</th>
-                      <td><a
-                          href=""
-                          target='_blank'>테스트</a> [9]
-                      </td>
-                      <td>작성자</td>
-                      <td>2022.09.21</td>
-                      <td>111</td>
-                      <td>111</td>
-                    </tr>
-                    <tr>
-                      <th scope="row">1</th>
-                      <td><a
-                          href=""
-                          target='_blank'>테스트</a> [9]
-                      </td>
-                      <td>작성자</td>
-                      <td>2022.09.21</td>
-                      <td>111</td>
-                      <td>222</td>
-                    </tr>
+                      <tr v-for="post in noticePosts.slice(0, 4)" :key="post.id">
+                        <th scope="row">{{ post.id }}</th>
+                        <td>
+                          <a class="text-link text-body" :href="`/boards/views/${post.id}`">{{ post.title }}</a>
+                          <label v-show="post.comment_count !== 0" class="text-danger">&nbsp;[{{ post.comment_count }}]</label>
+                        </td>
+                        <td>{{ post.author }}</td>
+                        <td>{{ post.created_at.split(' ')[0] }}</td>
+                        <td>{{ post.view_count }}</td>
+                        <td>{{ post.like_count }}</td>
+                      </tr>
+
                     </tbody>
                   </table>
                 </div>
@@ -79,60 +47,35 @@
                 <div class="card-body">
                   <table class="table table-hover" style="text-align: center">
                     <thead>
-                    <tr>
-                      <th>번호</th>
-                      <th>제목</th>
-                      <th>작성자</th>
-                      <th>작성일</th>
-                      <th>조회수</th>
-                      <th>추천수</th>
-                    </tr>
+                      <tr>
+                        <th>번호</th>
+                        <th>제목</th>
+                        <th>작성자</th>
+                        <th>작성일</th>
+                        <th>조회수</th>
+                        <th>추천수</th>
+                      </tr>
                     </thead>
                     <tbody>
-                    <tr>
-                      <th scope="row">1</th>
-                      <td><a
-                          href=""
-                          target='_blank'>테스트</a> [9]
-                      </td>
-                      <td>작성자</td>
-                      <td>2022.09.21</td>
-                      <td>111</td>
-                      <td>222</td>
-                    </tr>
-                    <tr>
-                      <th scope="row">1</th>
-                      <td><a
-                          href=""
-                          target='_blank'>테스트</a> [9]
-                      </td>
-                      <td>작성자</td>
-                      <td>2022.09.21</td>
-                      <td>111</td>
-                      <td>222</td>
-                    </tr>
-                    <tr>
-                      <th scope="row">1</th>
-                      <td><a
-                          href=""
-                          target='_blank'>테스트</a> [9]
-                      </td>
-                      <td>작성자</td>
-                      <td>2022.09.21</td>
-                      <td>111</td>
-                      <td>222</td>
-                    </tr>
-                    <tr>
-                      <th scope="row">1</th>
-                      <td><a
-                          href=""
-                          target='_blank'>테스트</a> [9]
-                      </td>
-                      <td>작성자</td>
-                      <td>2022.09.21</td>
-                      <td>111</td>
-                      <td>222</td>
-                    </tr>
+                      <tr v-for="post in topPosts" :key="post.id">
+                        <th scope="row">{{ post.id }}</th>
+                        <td>
+                          <a class="text-link text-body" :href="`/boards/views/${post.id}`">{{ post.title }}</a>
+                          <label v-show="post.comment_count !== 0" class="text-danger">&nbsp;[{{ post.comment_count }}]</label>
+                        </td>
+                        <td>{{ post.author }}</td>
+                        <td>{{ post.created_at.split(' ')[0] }}</td>
+                        <td>{{ post.view_count }}</td>
+                        <td>{{ post.like_count }}</td>
+                        <!-- <td><a
+                            href=""
+                            target='_blank'>테스트</a> [9]
+                        </td>
+                        <td>작성자</td>
+                        <td>2022.09.21</td>
+                        <td>111</td>
+                        <td>222</td> -->
+                      </tr>
                     </tbody>
                   </table>
                 </div>
@@ -179,34 +122,21 @@
           <h4>바로가기</h4>
           <div class="btn-group btn-group-lg" role="group" aria-label="Large button group">
             <button type="button" class="btn btn-outline-dark btn-light">
-              <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-lightbulb"
-                   viewBox="0 0 16 16">
-                <path
-                    d="M2 6a6 6 0 1 1 10.174 4.31c-.203.196-.359.4-.453.619l-.762 1.769A.5.5 0 0 1 10.5 13a.5.5 0 0 1 0 1 .5.5 0 0 1 0 1l-.224.447a1 1 0 0 1-.894.553H6.618a1 1 0 0 1-.894-.553L5.5 15a.5.5 0 0 1 0-1 .5.5 0 0 1 0-1 .5.5 0 0 1-.46-.302l-.761-1.77a1.964 1.964 0 0 0-.453-.618A5.984 5.984 0 0 1 2 6zm6-5a5 5 0 0 0-3.479 8.592c.263.254.514.564.676.941L5.83 12h4.342l.632-1.467c.162-.377.413-.687.676-.941A5 5 0 0 0 8 1z"/>
-              </svg>
-              <br><br>식품안전지식<br><br>
+              <h2 class="mt-1"><i class="bi bi-lightbulb"></i></h2>
+              <h5 class="mt-3">식품<br>안전지식</h5>
             </button>
             <button type="button" class="btn btn-outline-dark btn-light">
-              <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-virus"
-                   viewBox="0 0 16 16">
-                <path
-                    d="M8 0a1 1 0 0 1 1 1v1.402c0 .511.677.693.933.25l.7-1.214a1 1 0 0 1 1.733 1l-.701 1.214c-.256.443.24.939.683.683l1.214-.701a1 1 0 0 1 1 1.732l-1.214.701c-.443.256-.262.933.25.933H15a1 1 0 1 1 0 2h-1.402c-.512 0-.693.677-.25.933l1.214.701a1 1 0 1 1-1 1.732l-1.214-.7c-.443-.257-.939.24-.683.682l.701 1.214a1 1 0 1 1-1.732 1l-.701-1.214c-.256-.443-.933-.262-.933.25V15a1 1 0 1 1-2 0v-1.402c0-.512-.677-.693-.933-.25l-.701 1.214a1 1 0 0 1-1.732-1l.7-1.214c.257-.443-.24-.939-.682-.683l-1.214.701a1 1 0 1 1-1-1.732l1.214-.701c.443-.256.261-.933-.25-.933H1a1 1 0 1 1 0-2h1.402c.511 0 .693-.677.25-.933l-1.214-.701a1 1 0 1 1 1-1.732l1.214.701c.443.256.939-.24.683-.683l-.701-1.214a1 1 0 0 1 1.732-1l.701 1.214c.256.443.933.261.933-.25V1a1 1 0 0 1 1-1Zm2 5a1 1 0 1 0-2 0 1 1 0 0 0 2 0ZM6 7a1 1 0 1 0-2 0 1 1 0 0 0 2 0Zm1 4a1 1 0 1 0 0-2 1 1 0 0 0 0 2Zm5-3a1 1 0 1 0-2 0 1 1 0 0 0 2 0Z"/>
-              </svg>
-              <br><br>알레르기 정보<br><br></button>
+              <h2 class="mt-1"><i class="bi bi-virus"></i></h2>
+              <h5 class="mt-3">알레르기<br>정보</h5>
+            </button>
             <button type="button" class="btn btn-outline-dark btn-light">
-              <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor"
-                   class="bi bi-capsule-pill" viewBox="0 0 16 16">
-                <path
-                    d="M11.02 5.364a3 3 0 0 0-4.242-4.243L1.121 6.778a3 3 0 1 0 4.243 4.243l5.657-5.657Zm-6.413-.657 2.878-2.879a2 2 0 1 1 2.829 2.829L7.435 7.536 4.607 4.707ZM12 8a4 4 0 1 1 0 8 4 4 0 0 1 0-8Zm-.5 1.042a3 3 0 0 0 0 5.917V9.042Zm1 5.917a3 3 0 0 0 0-5.917v5.917Z"/>
-              </svg>
-              <br><br>식품안전의약처<br><br></button>
+              <h2 class="mt-1"><i class="bi bi-capsule-pill"></i></h2>
+              <h5 class="mt-3">식품안전<br>의약처</h5>
+            </button>
             <button type="button" class="btn btn-outline-dark btn-light">
-            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor"
-                 class="bi bi-capsule-pill" viewBox="0 0 16 16">
-              <path
-                  d="M11.02 5.364a3 3 0 0 0-4.242-4.243L1.121 6.778a3 3 0 1 0 4.243 4.243l5.657-5.657Zm-6.413-.657 2.878-2.879a2 2 0 1 1 2.829 2.829L7.435 7.536 4.607 4.707ZM12 8a4 4 0 1 1 0 8 4 4 0 0 1 0-8Zm-.5 1.042a3 3 0 0 0 0 5.917V9.042Zm1 5.917a3 3 0 0 0 0-5.917v5.917Z"/>
-            </svg>
-            <br><br>식중독 예방<br><br></button>
+              <h2 class="mt-1"><i class="bi bi-capsule-pill"></i></h2>
+              <h5 class="mt-3">식중독<br>예방</h5>
+            </button>
           </div>
           <p></p>
         </div>
@@ -243,23 +173,49 @@
 </template>
 
 <script>
-import {ref} from "vue";
+import { ref, onMounted } from 'vue'
+import useAxios from '@/modules/axios'
 
 export default {
   setup() {
+    const { axiosGet } = useAxios()
+
     const firstURL = ref('fo2vhh8cbfI')
     const secondURL = ref('b_MDW7gwnqg')
     const thirdURL = ref('h87bmuEvVlI')
+
+    const noticePosts = ref('')
+    const topPosts = ref('')
+
+    onMounted(() => {
+      // 공지사항 get
+      axiosGet('/api/v1/boards/1/posts', (res) => {
+        noticePosts.value = res.data
+      })
+
+      // TOP 게시글 get
+      axiosGet('/api/v1/boards/posts/top?size=4', (res) => {
+        topPosts.value = res.data
+      })
+    })
 
     return {
       firstURL,
       secondURL,
       thirdURL,
+      noticePosts,
+      topPosts,
     }
   }
 }
 </script>
 
 <style scoped>
-/* @import '@glidejs/glide/dist/css/glide.core.min.css'; */
+.text-link {
+  text-decoration: none;
+}
+
+.text-link:hover {
+  text-decoration: underline;
+}
 </style>
